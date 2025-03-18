30df40492f42a83ad5f78b614dfaaaf165d58a28
samay912 <samaysurya912@gmail.com>
2025-03-18 15:57:46 +0530

    Pipeline

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
+  cout<< "Auto build using Jenkins\n";
   cout << "I have successfully built and run this  \n";
   return 0;
 }
